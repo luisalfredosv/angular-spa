145fed304da442cc0fbb303aba74fe450c804df0
Luis Alfredo SV <luissarabia80@gmail.com>
2020-12-14 19:22:12 -0400

    Confimar en master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Router
+import { APP_ROUTING } from './app.routes';
+
+// Servicios
+import { HeroesService } from "./services/heroes.service";
+
+
+// Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Aqui van los componentes
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Aqui va el router
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    // Aqui van los servicios 
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
